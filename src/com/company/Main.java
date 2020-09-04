47da610d69e37bf73921ee6a7a505394b99ec9be
GorkovSer <30434820+GorkovSer@users.noreply.github.com>
2020-09-04 15:20:02 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,52 @@ class Boll
     public int getRad() {
         return rad;
     }
-
     public void setRad(int rad) {
         this.rad = rad;
     }
 }
+
+class Dog
+{
+    private String scale;
+
+    public String getScale() {
+        return scale;
+    }
+
+    public void setScale(String scale) {
+        this.scale = scale;
+    }
+}
+
+class Book
+{
+    private int numberOfPage;
+
+    public int getNumberOfPage() {
+        return numberOfPage;
+    }
+
+    public void setNumberOfPage(int numberOfPage) {
+        this.numberOfPage = numberOfPage;
+    }
+}
+
 
 public class Main {
 
     public static void main(String[] args) {
 
         Boll f = new Boll();
+        Dog a = new Dog();
+        Book b = new Book();
+
         f.setRad(10);
-        System.out.println(f.getRad());
+        a.setScale("big");
+        b.setNumberOfPage(1337);
+        System.out.println(
+                "Радиус мяча: " + f.getRad()+"\n" +
+                "Размер собаки: " + a.getScale()+"\n"+
+                "Кол-во страний: " + b.getNumberOfPage());
     }
 }
